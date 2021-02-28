9c41d2af7776cf83259b40b3268fae83d5091d8e
ShowWind846 <14k2132.sos@gmail.com>
2021-02-28 08:06:46 +0000

    [start]sign-up-page create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'public/homes#top'
+
+  namespace :public do
+    get 'about',to: 'homes#about'
+  end
+
 end
